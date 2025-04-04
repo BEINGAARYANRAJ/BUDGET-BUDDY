a77d99d2da453f2a549e9d62adba20a67bb2078b
BEINGAARYANRAJ <aryanraj3614@gmail.com>
2025-04-05 00:16:07 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Customers from './pages/Customers';
+import Dashboard from './components/Dashboard';
+import Customers from './components/Customers';
 import Products from './components/Products';
 import Reports from './components/Reports';
 import Settings from './components/Settings';
